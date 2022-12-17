4c3246c427c5cc522e55327feb9450912a55742b
deveq <deveq@kakao.com>
2022-12-18 00:30:59 +0900

    :sparkles: feat: add Service

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <nav>
       <div className="container nav__container">
         <a href="/" className="nav__logo">
-          <img className="img" src={Logo} alt="Logo" />
+          <img src={Logo} alt="Logo" />
         </a>
         <ul className="nav__menu">
           {data.map((item) => (
